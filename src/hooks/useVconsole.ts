c3fcbe69f2956369aabc08647c9b2626cc218c08
herzorf <1446450047@qq.com>
2025-03-22 01:06:36 +0800

    feat: 删除手机端调试工具

--- src/hooks/useVconsole.ts
+++
@@ -1,8 +0,0 @@
-import VConsole from 'vconsole'
-export function useVcosole() {
-  const vc = useRef<VConsole | null>(null)
-  if (location.href.includes('#vc')) {
-    vc.current = new VConsole({ theme: 'dark', maxLogNumber: 1000 })
-  }
-  return [vc.current] as const
-}
--- src/layout/default.tsx
+++ src/layout/default.tsx
@@ -1,16 +1,6 @@
 import { Outlet } from 'react-router-dom'
 
-import { useVcosole } from '@/hooks/useVconsole'
-// 这个是全局的页面 还可以做一些其他的操作
-
 export default function RootLayout() {
-  const [vc] = useVcosole()
-  useEffect(() => {
-    console.log('VConsole ?', vc)
-    if (vc) {
-      vc.show()
-    }
-  }, [])
   return (
     <div id={'root-layout'}>
       <Outlet />
