704d546a72a214bfe78281153f5a92e00f340558
herzorf <1446450047@qq.com>
2025-03-31 20:29:48 +0800

    feat: 更改登录接口地址

--- src/views/login/index.api.ts
+++ src/views/login/index.api.ts
@@ -1,7 +1,7 @@
 import { request } from '@/network/axios'
 
 enum APIS {
-  LOGIN = '/api/v1/login',
+  LOGIN = '/api/login',
 }
 
 export const login = (params: Record<string, string>) => request.post(APIS.LOGIN, params)
