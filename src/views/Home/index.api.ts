245bb2b689b1ebe0f44bc26dc34939ef5698633a
herzorf <1446450047@qq.com>
2025-04-01 21:39:12 +0800

    feat: 添加操作员接口调试

--- src/views/Home/index.api.ts
+++ src/views/Home/index.api.ts
@@ -2,8 +2,12 @@ import { request } from '@/network/axios'
 
 enum Api {
   queryOperatorList = '/api/operator/queryOperatorList',
+  createOperator = '/api/operator/createOperator',
 }
 
 export const queryOperatorList = (params: Record<string, string>) => {
   return request.post(Api.queryOperatorList, params)
+}
+export const createOperator = (params: Record<string, string>) => {
+  return request.post(Api.createOperator, params)
 }
--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -1,14 +1,18 @@
-import { Button, Table } from 'antd'
+import { Button, Form, Input, Modal, Switch, Table } from 'antd'
 
-import { queryOperatorList } from '@/views/Home/index.api.ts'
+import { createOperator, queryOperatorList } from '@/views/Home/index.api.ts'
 
 function Home() {
   const [dataSource, setDataSource] = useState<Record<string, string>[]>([])
+  const [isModalOpen, setIsModalOpen] = useState(false)
+
   useEffect(() => {
     queryOperatorList({}).then((res) => {
       setDataSource(res as Record<string, string>[])
     })
   }, [])
+  const [form] = Form.useForm()
+
   const columns = [
     {
       title: '用户id',
@@ -43,7 +47,54 @@ function Home() {
       render: () => <Button type="primary">操作</Button>,
     },
   ]
-  return <Table dataSource={dataSource} rowKey={(record) => record.id} columns={columns} />
+  type FieldType = {
+    username?: string
+    password?: string
+    isActive?: string
+  }
+
+  const onFinish = async () => {
+    const values = await form.validateFields()
+    await createOperator(values)
+    form.resetFields()
+    setIsModalOpen(false)
+  }
+
+  return (
+    <>
+      <Table
+        dataSource={dataSource}
+        title={() => (
+          <Button type="primary" onClick={() => setIsModalOpen(true)}>
+            添加操作员
+          </Button>
+        )}
+        rowKey={(record) => record.id}
+        columns={columns}
+      />
+      <Modal
+        centered
+        title="添加操作员"
+        open={isModalOpen}
+        okText="添加"
+        cancelText="取消"
+        onOk={onFinish}
+        onCancel={() => setIsModalOpen(false)}
+      >
+        <Form form={form} initialValues={{ isActive: true }} autoComplete="off">
+          <Form.Item<FieldType> label="账号" name="username" rules={[{ required: true, message: '请输入账号' }]}>
+            <Input />
+          </Form.Item>
+          <Form.Item<FieldType> label="密码" name="password" rules={[{ required: true, message: '请输入密码' }]}>
+            <Input.Password />
+          </Form.Item>
+          <Form.Item<FieldType> name="isActive" rules={[{ required: true }]} valuePropName="checked" label="是否激活">
+            <Switch />
+          </Form.Item>
+        </Form>
+      </Modal>
+    </>
+  )
 }
 
 export default Home
